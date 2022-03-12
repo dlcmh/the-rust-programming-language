b7758f603bd1be5c69f98a9f99995d76684e0e87
David Chin <dlcmhd@me.com>
2022-03-12 10:49:53 +0800

    features

--- 03-common-programming-concepts/3-3-functions/README.md
+++ 03-common-programming-concepts/3-3-functions/README.md
@@ -5,3 +5,10 @@ Function names conventionally use _snake case_.
 Parameters are special variables that are part of a function's signature. Each parameter must have a type.
 
 The values that are passed to a function are known as "arguments".
+
+Function bodies consist of a series of statements that optionally end in an expression.
+
+Rust is an expression-based language:
+
+- statements are instructions that perform some action and do not return a value
+- expressions evaluate to a resulting value
--- 03-common-programming-concepts/3-3-functions/functions/src/main.rs
+++ 03-common-programming-concepts/3-3-functions/functions/src/main.rs
@@ -3,7 +3,30 @@ fn main() {
 
     that_function(); // x is 2
 
-    print_labeled_measurement(10, 'h') // The measurement is: 10h
+    print_labeled_measurement(10, 'h'); // The measurement is: 10h
+
+    let _y = 6; // this is an example of a statement
+
+    // ## Because statements don't return values,
+    //   it's not allowed to assign the following `let _x = 6` statement to _w
+    //   as there's nothing for _w to bind to.
+    //   In C & Ruby, x = y = 6 is allowed, but not in Rust.
+    // let _w = (let _x = 6);
+    // error: expected expression, found statement (`let`)
+    // --> src/main.rs:12:15
+    //  |
+    //  |     let _w = (let _x = 6);
+    //  |               ^^^^^^^^^^
+    //  |
+    //  = note: variable declaration using `let` is a statement
+
+    // ## A new scope block created with curly brackets
+    //   is an example of an expression (because a value is returned).
+    let z = {
+        let x = 3;
+        x + 1
+    };
+    println!("z is {}", z); // z is 4
 }
 
 fn this_function() {}
@@ -15,27 +38,27 @@ fn that_function() {
 }
 
 // x is a "parameter"
-// parameters must be typed, otherwise, `fn those_functions(x) {// ...}` results in:
-//   error: expected one of `:`, `@`, or `|`, found `)`
-//     --> src/main.rs:14:21
-//      |
-//      | fn those_functions(x) {
-//      |                     ^ expected one of `:`, `@`, or `|`
-//      |
-//      = note: anonymous parameters are removed in the 2018 edition (see RFC 1685)
-//   help: if this is a `self` type, give it a parameter name
-//      |
-//      | fn those_functions(self: x) {
-//      |                    +++++
-//   help: if this is a parameter name, give it a type
-//      |
-//      | fn those_functions(x: TypeName) {
-//      |                     ++++++++++
-//   help: if this is a type, explicitly ignore the parameter name
-//      |
-//      | fn those_functions(_: x) {
-//      |
 fn those_functions(x: i32) {
+    // parameters must be typed, otherwise, `fn those_functions(x) {// ...}` results in:
+    //   error: expected one of `:`, `@`, or `|`, found `)`
+    //     --> src/main.rs:14:21
+    //      |
+    //      | fn those_functions(x) {
+    //      |                     ^ expected one of `:`, `@`, or `|`
+    //      |
+    //      = note: anonymous parameters are removed in the 2018 edition (see RFC 1685)
+    //   help: if this is a `self` type, give it a parameter name
+    //      |
+    //      | fn those_functions(self: x) {
+    //      |                    +++++
+    //   help: if this is a parameter name, give it a type
+    //      |
+    //      | fn those_functions(x: TypeName) {
+    //      |                     ++++++++++
+    //   help: if this is a type, explicitly ignore the parameter name
+    //      |
+    //      | fn those_functions(_: x) {
+    //      |
     println!("x is {}", x);
 }
 
