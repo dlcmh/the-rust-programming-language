44d8a457f12a58bade918dff0e5ef58a45e0a7e4
David Chin <dlcmhd@me.com>
2022-03-18 16:00:51 +0800

    features

--- 06-enums/6-2-match/crates/us_coins/src/main.rs
+++ 06-enums/6-2-match/crates/us_coins/src/main.rs
@@ -7,7 +7,10 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
@@ -19,6 +22,7 @@ fn main() {
     println!("{}", value_in_cents(Coin::Nickel));
     println!("{}", value_in_cents(Coin::Dime));
     println!("{}", value_in_cents(Coin::Quarter));
+    // Lucky penny!
     // 1
     // 5
     // 10
