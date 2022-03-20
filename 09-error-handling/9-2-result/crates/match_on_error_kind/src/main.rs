4096c43eff026d1407e69d2f56597a96595a3839
David Chin <dlcmhd@me.com>
2022-03-20 15:18:53 +0800

    --wip-- [skip ci]

--- 09-error-handling/9-2-result/crates/match_on_error_kind/src/main.rs
+++ 09-error-handling/9-2-result/crates/match_on_error_kind/src/main.rs
@@ -1,3 +1,28 @@
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                println!("Creating file...");
+                match File::create("hello.txt") {
+                    Ok(fc) => {
+                        println!("File created!");
+                        fc
+                    }
+                    Err(e) => panic!("Problem creating file: {:?}", e),
+                }
+            }
+            other_error => panic!("Problem opening file: {:?}", other_error),
+        },
+    };
+    println!("File handle: {:#?}", f);
+    // File handle: File {
+    //     fd: 3,
+    //     path: "/Users/dlcmh/projects/the-rust-programming-language/09-error-handling/9-2-result/crates/match_on_error_kind/hello.txt",
+    //     read: true,
+    //     write: false,
+    // }
 }
