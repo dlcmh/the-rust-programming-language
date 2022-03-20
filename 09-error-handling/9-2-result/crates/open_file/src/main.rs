db29bb3aca004406ef26ffa114349437ad2e4eb7
David Chin <dlcmhd@me.com>
2022-03-20 09:38:21 +0800

    --wip-- [skip ci]

--- 09-error-handling/9-2-result/crates/open_file/src/main.rs
+++ 09-error-handling/9-2-result/crates/open_file/src/main.rs
@@ -2,5 +2,12 @@ use std::fs::File;
 
 fn main() {
     // f is inferred as type Result<File, Error>
+    // `File` is a file handle that we can read from or write to
     let f = File::open("no-such-file.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+    // thread 'main' panicked at 'Problem opening the file: Os { code: 2, kind: NotFound, message: "No such file or directory" }', src/main.rs:10:23
 }
