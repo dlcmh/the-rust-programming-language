e5bbe9932d244e029f742950826e0a873c5d3a45
David Chin <dlcmhd@me.com>
2022-03-27 18:17:58 +0800

    --wip-- [skip ci]

--- 12-io-cli-program/minigrep/src/main.rs
+++ 12-io-cli-program/minigrep/src/main.rs
@@ -1,8 +1,8 @@
-use std::env;
+use std::{env, fs};
 
-// cargo run searchstring example-filename.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // cargo run searchstring example-filename.txt
     // println!("args: {:?}", args);
     // args: ["/Users/dlcmh/rust/target/debug/minigrep", "searchstring", "example-filename.txt"]
 
@@ -10,6 +10,10 @@ fn main() {
     let filename = &args[2];
     println!("Searching for {}", query);
     println!("In file {}", filename);
+    // cargo run searchstring example-filename.txt
     // Searching for searchstring
     // In file example-filename.txt
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
