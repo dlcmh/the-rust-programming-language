2970132046fe92bca9280a35a571d80aaabcd99c
David Chin <dlcmhd@me.com>
2022-03-29 16:40:32 +0800

    --wip-- [skip ci]

--- 13-functional-language-features/13-1-closures/01-abstract-behavior/workout_generator/src/workout.rs
+++ 13-functional-language-features/13-1-closures/01-abstract-behavior/workout_generator/src/workout.rs
@@ -6,24 +6,61 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
+// Iteration #3: Define a closure, and store this definition in a variable so that it can be called later.
+// This pattern seems similar to memoization - the first time `expensive_clousure` is called.
+// the result is stored inside the variable itself.
 pub fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| simulated_expensive_calculation(num);
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remeber to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
-            );
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
         }
     }
 }
+
+// Iteration #2 - refactor by storing the result of duplicated function calls in a variable.
+// Still not ideal as expensive result gets run at least once, although it's not
+// needed by the `random_number == 3` `if` block.
+// pub fn generate_workout(intensity: u32, random_number: u32) {
+//     let expensive_result = simulated_expensive_calculation(intensity);
+
+//     if intensity < 25 {
+//         println!("Today, do {} pushups!", expensive_result);
+//         println!("Next, do {} situps!", expensive_result);
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remeber to stay hydrated!");
+//         } else {
+//             println!("Today, run for {} minutes!", expensive_result);
+//         }
+//     }
+// }
+
+// Iteration #1 - expensive calculation called multiple times instead of once.
+// pub fn generate_workout(intensity: u32, random_number: u32) {
+//     if intensity < 25 {
+//         println!(
+//             "Today, do {} pushups!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//         println!(
+//             "Next, do {} situps!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remeber to stay hydrated!");
+//         } else {
+//             println!(
+//                 "Today, run for {} minutes!",
+//                 simulated_expensive_calculation(intensity)
+//             );
+//         }
+//     }
+// }
