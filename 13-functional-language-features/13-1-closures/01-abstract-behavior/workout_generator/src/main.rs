09629a4ae5150e8d079c422ec90dc3dc3954afd6
David Chin <dlcmhd@me.com>
2022-03-29 15:52:22 +0800

    --wip-- [skip ci]

---
+++ 13-functional-language-features/13-1-closures/01-abstract-behavior/workout_generator/src/main.rs
@@ -0,0 +1,8 @@
+use workout_generator::workout;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    workout::generate_workout(simulated_user_specified_value, simulated_random_number);
+}
---
+++ 13-functional-language-features/13-1-closures/01-abstract-behavior/workout_generator/src/workout.rs
@@ -0,0 +1,29 @@
+use std::{thread, time::Duration};
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2)); // like timeout, timer
+    intensity
+}
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remeber to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
+}
