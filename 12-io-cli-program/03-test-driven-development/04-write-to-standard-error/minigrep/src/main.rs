1ed81a203cc6c3e0b8f613e2f7d2e0b55520b6b0
David Chin <dlcmhd@me.com>
2022-03-29 12:47:11 +0800

    --wip-- [skip ci]

--- 12-io-cli-program/03-test-driven-development/04-write-to-standard-error/minigrep/src/main.rs
+++ 12-io-cli-program/03-test-driven-development/04-write-to-standard-error/minigrep/src/main.rs
@@ -14,16 +14,19 @@ use minigrep::Config;
 // I'm nobody! Who are you?
 // Are you nobody, too?
 // How dreary to be somebody!
+//
+// (A) cargo run > output.txt - no content in output.txt, error displayed on the screen
+// (B) cargo run -q frog ../../../poem.txt > output.txt - content in output.txt, nothig displayed on the screen
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
