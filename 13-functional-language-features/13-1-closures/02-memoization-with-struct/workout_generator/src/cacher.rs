b345b5749a6fb73c403231c41e2d97a5235b569e
David Chin <dlcmhd@me.com>
2022-04-10 12:55:02 +0800

    --wip-- [skip ci]

--- 13-functional-language-features/13-1-closures/02-memoization-with-struct/workout_generator/src/cacher.rs
+++ 13-functional-language-features/13-1-closures/02-memoization-with-struct/workout_generator/src/cacher.rs
@@ -1,8 +1,14 @@
+// `Cacher` struct:
+// - holds a closure in `calculation` field
+//   - `calculation` field is of the generic type `T`
+//   - trait bounds on `T` specify that it's a closure by using the `Fn` trait
+//   - closure stored on `calculation` field must have on `u32` parameter and
+//     must return a `u32`
+// - holds an optional result in `value` field
 pub struct Cacher<T>
 where
     T: Fn(u32) -> u32,
 {
-    // trait bounds on `T` specify that `calculation` field is a closure by using the `Fn` trait
     pub calculation: T,
     pub value: Option<u32>,
 }
