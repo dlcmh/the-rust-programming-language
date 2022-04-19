2544414784398f9862f65f56a773ad7db4457f52
David Chin <dlcmhd@me.com>
2022-04-19 15:30:05 +0800

    --wip-- [skip ci]

--- 13-functional-language-features/13-1-closures/02-memoization-with-struct/workout_generator/src/cacher.rs
+++ 13-functional-language-features/13-1-closures/02-memoization-with-struct/workout_generator/src/cacher.rs
@@ -9,13 +9,39 @@ where
     //   - trait bounds on `T` specify that it's a closure by using the `Fn` trait
     //   - closure stored on `calculation` field must have on `u32` parameter and
     //     must return a `u32`
-    pub calculation: T,
+    calculation: T,
 
+    //   The following logic is defined in `impl<T> Cacher<T>`:
     //   - before the closure is executed, `value` will be `None`
     //   - when code using a `Cacher` askd for the result of the closure, the `Cacher`
     //     will execute the closure and store the result within a `Some` variant in the
     //     `value` field
     //   - if the code asks for the result of the clousure again, instead of executing the
     //     closure again, the `Cacher` will return the result held in the `Some` variant
-    pub value: Option<u32>,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    // takes a generic parameter `T`, which is defined as having the same trait bound
+    // as the `Cacher` struct
+    pub fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    pub fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
--- 13-functional-language-features/13-1-closures/02-memoization-with-struct/workout_generator/src/main.rs
+++ 13-functional-language-features/13-1-closures/02-memoization-with-struct/workout_generator/src/main.rs
@@ -1,8 +1,20 @@
 use workout_generator::workout;
 
 fn main() {
+    // calculating slowly...
+    // Today, do 10 pushups!
+    // Next, do 10 situps!
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
+
+    // calculating slowly...
+    // Today, run for 100 minutes!
+    // let simulated_user_specified_value = 100;
+    // let simulated_random_number = 7;
+
+    // Take a break today! Remeber to stay hydrated!
+    // let simulated_user_specified_value = 100;
+    // let simulated_random_number = 3;
 
     workout::generate_workout(simulated_user_specified_value, simulated_random_number);
 }
